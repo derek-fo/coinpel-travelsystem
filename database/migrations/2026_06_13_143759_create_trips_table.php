31c05c1cf94fc0a74c20eaebcb4c20394d04d9c1
Derek Oliveira <derek.foliveira@gmail.com>
2026-06-13 11:40:00 -0300

    tabela de viagens criada
    utilizando o mesmo comando para criar as tabelas passadas foi criado uma nova tabela para viagens

---
+++ database/migrations/2026_06_13_143759_create_trips_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->string('origin'); // cidade de origem
+            $table->string('destination'); // cidade de destino
+
+            $table->dateTime('departure_time'); // data e hora de partida
+            $table->dateTime('arrival_time'); // data e hora de chegada
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trips');
+    }
+};
